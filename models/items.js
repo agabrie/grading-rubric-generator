7d219de6a10ed8d05845913153b5eec2abb8b408
abduraghmaan <abduraghmaangabriels2@gmail.com>
2019-10-08 19:04:56 +0200

    added models for project

--- models/items.js
+++ models/items.js
@@ -12,6 +12,109 @@ const GeoSchema = new Schema({
 	}
 });
 
+const LinkSchema = new Schema({
+	url:{
+		type:String,
+		required:[true]
+	},
+	description:{
+		type:String
+	}
+
+});
+
+const TaskSchema= new Schema({
+	name:{
+		type:String,
+		required:[true, 'Taskname required']
+	},
+	description:{
+		type:String
+	},
+	links:{
+		type:[LinkSchema]
+	},
+	completed:{
+		type:Boolean
+	},
+	priority:{
+		type:Number
+	},
+	timeStamp:{
+		type:Date
+	}
+});
+
+const ComponentSchema= new Schema({
+	name:{
+		type:String,
+		required:[true, 'Taskname required']
+	},
+	description:{
+		type:String
+	},
+	links:{
+		type:[LinkSchema]
+	},
+	// completed:{
+	// 	type:Boolean
+	// },
+	tasks:{
+		type:[TaskSchema]
+	},
+	priority:{
+		type:Number
+	},
+	timeStamp:{
+		type:Date
+	}
+});
+const SectionSchema= new Schema({
+	name:{
+		type:String,
+		required:[true, 'Taskname required']
+	},
+	description:{
+		type:String
+	},
+	links:{
+		type:[LinkSchema]
+	},
+	// completed:{
+	// 	type:Boolean
+	// },
+	components:{
+		type:[ComponentSchema]
+	},
+	priority:{
+		type:Number
+	},
+	timeStamp:{
+		type:Date
+	}
+});
+const ProjectSchema= new Schema({
+	name:{
+		type:String,
+		required:[true, 'Taskname required']
+	},
+	description:{
+		type:String
+	},
+	links:{
+		type:[LinkSchema]
+	},
+	// completed:{
+	// 	type:Boolean
+	// },
+	sections:{
+		type:[SectionSchema]
+	},
+	timeStamp:{
+		type:Date
+	}
+});
+
 const ItemSchema = new Schema({
 	username:{
 		type: String,
@@ -37,5 +140,5 @@ const ItemSchema = new Schema({
 });
 
 const Item = mongoose.model('item', ItemSchema);
-
-module.exports = Item;
+const Project = mongoose.model('project',ProjectSchema)
+module.exports = {Item,Project};
